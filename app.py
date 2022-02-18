86de9e3b08e968e6a1389830b2c87398f355d311
Bruna Nicolato <86579549+bruninhanic@users.noreply.github.com>
2022-02-18 20:32:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ cnaeURL = "https://github.com/bruninhanic/riscoVisaMG/blob/main/cnaesVisa.csv?ra
 def load_data():
     # carrega os dados das atividades
 
-    data = pd.read_csv(cnaeURL, dtype='object', encoding='utf8', sep=';')
+    data = pd.read_csv(cnaeURL, dtype='object', encoding='utf8', sep=';', index_col='codigoCnae')
 
     return data
 
 df = load_data()
-labels = df['codigoCnae'].tolist()
+labels = df.index.tolist()
 
 #SIDEBAR
 
@@ -32,14 +32,14 @@ label_to_filter = st.sidebar.multiselect(
 st.sidebar.markdown('Para a correta classificação de risco, você deve informar todas as atividades sujeitas a controle sanitário.')
 
 #Aplicação do filtro no dataframe e classificação do risco
-filtered_df = df[df.codigoCnae.isin(label_to_filter)]
+filtered_df = df[df.index.isin(label_to_filter)]
 
 riscos = filtered_df.classificacaoRisco.to_list()
 
 def risco_estabelecimento():
     
     if not riscos:
-        classificacaoRisco = 'Selecione as atividades realizadas pelo estabelecimento'
+        classificacaoRisco = None
         return classificacaoRisco        
     if 'Nível de Risco III' in riscos:
         classificacaoRisco = 'Nível de Risco III'
