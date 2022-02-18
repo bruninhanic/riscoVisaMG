a4da15c468b6aced97869e7dddb929f24a2041c1
Bruna Nicolato <86579549+bruninhanic@users.noreply.github.com>
2022-02-18 19:09:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ cnaeURL = "https://github.com/bruninhanic/riscoVisaMG/blob/main/cnaesVisa.csv"
 def load_data():
     # carrega os dados das atividades
 
-    data = pd.read_csv(cnaeURL, dtype='object', index_col='codigoCnae', encoding='utf8', sep=';', on_bad_lines)
+    data = pd.read_csv(cnaeURL, dtype='object', index_col='codigoCnae', encoding='utf8', sep=';')
 
     return data
 
