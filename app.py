311662b1980772c7c40421ef9bc7f4ed33cafffd
Bruna Nicolato <86579549+bruninhanic@users.noreply.github.com>
2022-03-16 22:34:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def load_respostas():
 respostas_df = load_respostas()
 
 
-st.header('Consulta a classificação de risco sanitário de estabelecimentos segundo as normas sanitárias do Estado de Minas Gerais - Resolução SES/MG n. 7426/2021')
+st.header('Consulta a classificação do risco sanitário de estabelecimentos segundo a legislação vigente do Estado de Minas Gerais - Resolução SES/MG n. 7426/2021')
 
 st.markdown('Aviso 1:')
 st.caption('As classificações de risco da aplicação se referem ao disposto na Resolução SES/MG 7426/2021, aplicável aos estabelecimentos mineiros.')
