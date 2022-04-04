68e7d2ecd3977e8a068d85ce087d3527e97fa515
Bruna Nicolato <86579549+bruninhanic@users.noreply.github.com>
2022-04-04 10:12:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -380,8 +380,7 @@ if submit:
     remote_url = 'https://github.com/bruninhanic/riscoVisaMG/blob/main/AtividadesVisaAbril2022.txt'
     local_file = 'AtividadesVISAMG.txt'
 
-    ativ = wget.download(remote_url, local_file)
-return ativ
+    wget.download(remote_url, local_file)
 
 c = st.container()
 c.subheader('Deseja solicitar alguma alteração?')
