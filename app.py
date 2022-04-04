46ecf65042829f044ba8b79fa67869a6c9febdaf
Bruna Nicolato <86579549+bruninhanic@users.noreply.github.com>
2022-04-04 16:34:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -385,7 +385,7 @@ if submit:
     url = 'https://github.com/bruninhanic/riscoVisaMG/blob/main/AtividadesVisaAbril2022.txt'
     resp = requests.get(url)
     bs = BeautifulSoup(resp.content, 'html.parser')
-    ativ = bs.find_all('h1')
+    ativ = bs.find_all('p')
     s.text(ativ)
        
 
